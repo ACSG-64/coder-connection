8cd72121367885893e9627ac023b7840c1848b97
Andrés Sarmiento <acamilo707@gmail.com>
2023-09-11 04:30:02 -0500

    Using a link in the application name

--- src/components/site-icon.tsx
+++ src/components/site-icon.tsx
@@ -1,11 +1,12 @@
+import Link from 'next/link'
 import { Paragraph } from './ui/typography'
 import Image from 'next/image'
 
-export default function SiteIcon() {
+export default function SiteIcon({ href = '#' }: { href: string }) {
     return (
         <div className="flex flex-row-reverse items-center justify-end gap-2 px-2">
             <Paragraph className="m-0 p-0 font-extrabold">
-                CoderConnection
+                <Link href={href}>CoderConnection</Link>
             </Paragraph>
             <Image
                 className="drop-shadow"
